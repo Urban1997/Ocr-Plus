f200dbdb9ce7719e8a907d2c5287f4ae83664bd7
Urban1997 <155580062+Urban1997@users.noreply.github.com>
2026-04-07 22:24:20 -0600

    Principios OCR Plus en python

---
+++ Dashboard/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def hello(request):
+    return HttpResponse("<h1>Hola perrete</h1>")
+
+def about(request):
+    return HttpResponse("<h1>Sobre nosotros</h1>")
